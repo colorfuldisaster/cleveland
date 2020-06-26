2ae72e13f9824a89e14946bbe768944826ace596
colorfuldisaster <thecolorfuldisaster@gmail.com>
2020-06-27 02:04:57 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='cleveland',
+        version='1.0',
+        description='bot for multiplayer battleship game at https://github.com/colorfuldisaster/battleship',
+        license='MIT',
+        author='colorfuldisaster',
+        author_email='thecolorfuldisaster@gmail.com',
+        packages=['cleveland'],
+)
